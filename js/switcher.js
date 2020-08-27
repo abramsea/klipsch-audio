fab4c207688b7edadfe28e999b6990cfb3e2b030
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-08-27 15:29:23 +0300

    test

---
+++ js/switcher.js
@@ -0,0 +1,153 @@
+$('#b-floor').click(() => {
+    $("#floor").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-shelf').click(() => {
+    $("#shelf").show();
+    $('#floor').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-sub').click(() => {
+    $("#sub").show();
+    $('#floor').hide();
+    $('#shelf').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-central').click(() => {
+    $("#central").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#floor').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-soundbar').click(() => {
+    $("#soundbar").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#floor').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-atmos').click(() => {
+    $("#atmos").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#floor').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-wall').click(() => {
+    $("#wall").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#floor').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-system').click(() => {
+    $("#system").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#floor').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-portable').click(() => {
+    $("#portable").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#floor').hide();
+    $('#wireless').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-wireless').click(() => {
+    $("#wireless").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#floor').hide();
+    $('#home-cinema').hide();
+})
+
+$('#b-home-cinema').click(() => {
+    $("#home-cinema").show();
+    $('#shelf').hide();
+    $('#sub').hide();
+    $('#central').hide();
+    $('#soundbar').hide();
+    $('#atmos').hide();
+    $('#wall').hide();
+    $('#system').hide();
+    $('#portable').hide();
+    $('#wireless').hide();
+    $('#floor').hide();
+})
